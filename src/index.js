b5e86af64065247c320f8db63ce3f78cc0ef9b31
matheusgarcia37 <matheusgarcia37@hotmail.com>
2022-02-01 22:14:05 -0300

    att portfolio

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import GlobalStyle from './styles/GlobalStyle';
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
-    <BrowserRouter>
+    <BrowserRouter basename='my-portfolio'>
       <App />
     </BrowserRouter>
     
